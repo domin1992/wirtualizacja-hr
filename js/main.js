27d7608daad6d2dbf41732fa507eec2ca17d8756
Dominik Nowak <domin1204@gmail.com>
2016-12-28 14:14:00 +0100

    Added new sections including quiz

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    // init fullpage
     $('#fullpage').fullpage();
+
+    $('#mini-quiz .card').click(function(){
+        $('#answer_msg').removeClass('alert-success');
+        $('#answer_msg').removeClass('alert-danger');
+        if($(this).attr('id') == 'answer_3'){
+            $('#answer_msg').addClass('alert-success');
+            $('#answer_msg').text('Poprawna odpowiedź');
+            $('#answer_msg').fadeIn();
+        }
+        else{
+            $('#answer_msg').addClass('alert-danger');
+            $('#answer_msg').text('Niepoprawna odpowiedź');
+            $('#answer_msg').fadeIn();
+        }
+        $('#mini-quiz .card-cover').each(function(){
+            $(this).show();
+        });
+    });
 });
